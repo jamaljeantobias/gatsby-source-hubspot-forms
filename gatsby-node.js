816798d7f015312ff3faa42ad48df2fc061ff59b
Sean Proctor <sproctor@gmail.com>
2023-03-15 17:32:44 -0400

    add graphql type definitions

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,3 +39,64 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
     throw new Error(err)
   }
 }
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type HubspotFormFieldValidation {
+      name: String
+      message: String
+      data: String
+      useDefaultBlockList: Boolean
+      blockedEmailAddresses: [String]
+    }
+    
+    type HubspotFormField {
+      name: String!
+      label: String!
+      type: String!
+      fieldType: String!
+      description: String
+      groupName: String
+      displayOrder: Int
+      required: Boolean
+      selectedOptions: [String]
+      options: [String]
+      validation: HubspotFormFieldValidation
+      enabled: Boolean,
+      hidden: Boolean,
+      defaultValue: String
+      isSmartField: Boolean
+      unselectedLabel: String
+      placeholder: String
+      # dependentFieldFilters: array of some kind
+      labelHidden: Boolean
+    }
+    
+    type HubspotFormFieldGroup {
+      fields: [HubspotFormField]
+      default: Boolean
+      isSmartGroup: Boolean
+      # richText
+    }
+    
+    type HubspotForm implements Node {
+      id: ID!
+      portalId: Int!
+      guid: String!
+      name: String!
+      action: String
+      method: String
+      cssClass: String
+      redirect: String
+      submitText: String
+      followUpId: String
+      notifyRecipients: String
+      leadNurturingCampaignId: String
+      formFieldGroups: [HubspotFormFieldGroup]
+      inlineMessage: String
+      isPublished: Boolean
+    }
+  `
+  createTypes(typeDefs)
+}
